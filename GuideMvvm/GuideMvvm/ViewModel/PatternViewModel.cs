2ce8ab0e530218e719dd09328f70d451ad00a255
juniorsaraviao <junior.saravia.o@uni.pe>
2022-08-15 23:07:32 -0500

    Add PatternViewModel

---
+++ GuideMvvm/GuideMvvm/ViewModel/PatternViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GuideMvvm.ViewModel
+{
+   public class PatternViewModel : BaseViewModel
+   {
+      string text;
+
+      public ICommand AsyncProcessCommand { get; }
+      public ICommand SimpleProcessCommand { get; }
+
+      public PatternViewModel(INavigation navigation)
+      {
+         Navigation = navigation;
+         AsyncProcessCommand = new Command(async () => await AsyncProcess());
+         SimpleProcessCommand = new Command(SimpleProcess);
+      }
+
+      public string Text
+      {
+         get => text;
+         set => SetProperty(ref text, value);
+      }
+
+      public async Task AsyncProcess()
+      {
+
+      }
+
+      void SimpleProcess()
+      {
+
+      }
+   }
+}
