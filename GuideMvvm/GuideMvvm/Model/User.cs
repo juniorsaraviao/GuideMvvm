e14df629040769755bb869548ad5e0cf4a1b26a5
juniorsaraviao <junior.saravia.o@uni.pe>
2022-08-28 21:58:54 -0500

    Use CollectionView

---
+++ GuideMvvm/GuideMvvm/Model/User.cs
@@ -0,0 +1,8 @@
+﻿namespace GuideMvvm.Model
+{
+   public class User
+   {
+      public string Name { get; set; }
+      public string Image { get; set; }
+   }
+}
--- GuideMvvm/GuideMvvm/ViewModel/SecondPageViewModel.cs
+++ GuideMvvm/GuideMvvm/ViewModel/SecondPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using GuideMvvm.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -6,16 +8,47 @@ namespace GuideMvvm.ViewModel
 {
    public class SecondPageViewModel : BaseViewModel
    {
+      public List<User> UserList { get; set; }
       public ICommand BackCommand { get; }
+      public ICommand AlertCommand { get; }
       public SecondPageViewModel(INavigation navigation)
       {
          Navigation = navigation;
          BackCommand = new Command(async () => await BackNavigation());
+         AlertCommand = new Command(async () => await AlertMessage());
+         AddUsers();
       }
 
       async Task BackNavigation()
       {
          await Navigation.PopAsync();
+      }
+
+      async Task AlertMessage()
+      {
+         await DisplayAlert("Title", "Message", "Ok");
+      }
+
+      void AddUsers()
+      {
+         UserList = new List<User>
+         {
+            new User
+            {
+               Name = "User 1",
+               Image = "https://i.ibb.co/gvpcDWw/pescado.png"
+            },
+            new User
+            {
+               Name = "User 2",
+               Image = "https://i.ibb.co/RSrm1rJ/pulpo.png"
+            },
+            new User
+            {
+               Name = "User 3",
+               Image = "https://i.ibb.co/RSrm1rJ/pulpo.png"
+            }
+         };
       }
    }
 }
